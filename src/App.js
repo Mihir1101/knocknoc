75a755effcfa7c3a4219ae1903bcbc84457fc94c
Mihir <mihir6453@gmail.com>
2023-08-27 21:55:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import lighthouse from '@lighthouse-web3/sdk';
 
 function App() {
+  
+  const progressCallback = (progressData) => {
+    let percentageDone =
+    100 - (progressData?.total / progressData?.uploaded)?.toFixed(2);
+    console.log(percentageDone);
+  };
+  const [files, setfiles] = useState([]);
+  var allFiles = [];
+  
+  const uploadFile = async(file) =>{
+    const output = await lighthouse.upload(file, '8976353a.fca13ed3f5e94bfda9383dcd3e51aa9b', false, null, progressCallback);
+    console.log('File Status:', output);
+    console.log('Visit at https://gateway.lighthouse.storage/ipfs/' + output.data.Hash);
+    allFiles.push(output.data);
+    localStorage.setItem('files', JSON.stringify(allFiles));
+    setfiles(JSON.parse(localStorage.getItem('files')));
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onChange={e=>uploadFile(e.target.files)} type="file" />
+      {
+        files.map((file, index) => {
+          return (
+            <div key={index}>
+              <p>File name : {file.Name}</p>
+              <p>File hash : {file.Hash}</p>
+            </div>
+          )
+        })
+      } 
+      <h1>Upload File</h1>
+      
     </div>
   );
 }

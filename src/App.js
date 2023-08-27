3309745b6d05afbe945627071f403cacab8ca242
the-geek-deep <deepumrajkar15@gmail.com>
2023-08-28 03:37:15 +0530

    feat: completed the project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+
+
 import React from "react";
 import { useState, useEffect } from "react";
 import lighthouse from '@lighthouse-web3/sdk';
+import "./App.css";
 
 function App() {
   
@@ -13,7 +16,7 @@ function App() {
   var allFiles = [];
   
   const uploadFile = async(file) =>{
-    const output = await lighthouse.upload(file, '8976353a.fca13ed3f5e94bfda9383dcd3e51aa9b', false, null, progressCallback);
+    const output = await lighthouse.upload(file, process.env.YOUR_LIGHTHOUSE_API_KEY, false, null, progressCallback);
     console.log('File Status:', output);
     console.log('Visit at https://gateway.lighthouse.storage/ipfs/' + output.data.Hash);
     allFiles.push(output.data);
@@ -21,21 +24,39 @@ function App() {
     setfiles(JSON.parse(localStorage.getItem('files')));
   }
   
+  
   return (
+    // <Widgetsrc = "https://near.org/near/widget/ComponentDetailsPage?src=shelby1508.near/widget/Onboarding.Starter"></Widgetsrc>
+
+    /* Login with BOS IDE vs code extension after making your near wallet to run the frontend decentralized */
+
+
     <div className="App">
-      <input onChange={e=>uploadFile(e.target.files)} type="file" />
-      {
-        files.map((file, index) => {
-          return (
-            <div key={index}>
-              <p>File name : {file.Name}</p>
-              <p>File hash : {file.Hash}</p>
-            </div>
-          )
-        })
-      } 
-      <h1>Upload File</h1>
-      
+      <header className="App-header">
+        <h1 className="app-title">KnocKnoc</h1>
+        <div className="input-wrapper">
+          <label className="publish-label" htmlFor="file-input">Publish Music</label>
+          <input id="file-input" onChange={e => uploadFile(e.target.files)} type="file" acccept = "audio/mp3"  className="file-input" />
+        </div>
+        <div className="file-list">
+          {
+            files.map((file, index) => (
+              <div className="file-info" key={index}>
+               
+                <p ><strong>File name:</strong> {file.Name}</p>
+                <p ><strong>File Hash :</strong> {file.Hash}</p>
+               
+                <div className="link">
+                <p><a href = {"https://gateway.lighthouse.storage/ipfs/" + file.Hash }>Download File</a></p>
+                </div>
+
+
+              </div>
+            ))
+          }
+        </div>
+       
+      </header>
     </div>
   );
 }
